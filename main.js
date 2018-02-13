517897c49a2b42cbd4ad94bbe9f0da8572cda6f6
patrik909 <patrik.eriksson@medieinstitutet.se>
2018-02-13 20:22:08 +0100

    added complete, remove, remove all function

--- main.js
+++ main.js
@@ -1,35 +1,124 @@
 const whatTodo = document.getElementById('whatTodo');
 const newTodo = document.getElementById('newTodo');
+const messageBox = document.getElementById('messageBox');
 
 const todoList = document.getElementById('todos');
 const doneList = document.getElementById('dones');
+const deleteAll = document.getElementById('deleteAll');
 
 const todoArray = [];
-//const doneArray = ["Volvo", "Villa", "Fru", "Vovve"];
-
-/* FÖR ATT KOLLA OM strängen finns
-var exists = todoArray.includes('en sträng');
-
-if(exists == true){
-    console.log("YES")
-} else {
-    console.log("NO")
-} */
+const doneArray = [];
 
 /* --------------------------- */
 /* ------- ADDA EN TODO ------ */
 /* --------------------------- */
 
-newTodo.addEventListener('submit', addTodo)
-// antaingen göra en funktion som lägger till det senaste som lagts till i arrayen till loopen, just nu appendas allt om och om igen.
-function addTodo(event){
+newTodo.addEventListener('submit', function(){
+    addTodo(event);
+})
+
+function addTodo(){
+
     event.preventDefault();
-    todovalue = whatTodo.value;
-    todoArray.push(todovalue);
-    console.log(todoArray);
+    const todoValue = whatTodo.value;
+    if(todoValue == ""){
+        messageBox.innerHTML=`
+            <p>DU MATADE INTE IN NÅGOT</p>
+        `   
+    } else {
+        messageBox.innerHTML="";
+        todoArray.push(todoValue);
+    }
+    
     viewTodo();
+
+} 
+
+///---------------------------///
+
+/* ------------------------------ */
+/* --------- UTFÖRD TODO -------- */
+/* ------------------------------ */
+
+function completeTodo(){
+    this.parentElement.remove();
+    
+    const todoBox = this.parentElement
+    const todoBoxParagraph = todoBox.getElementsByTagName('p')[0];
+    const value = todoBoxParagraph.textContent;
+    doneArray.push(value);
+    
+    var searchValue = todoArray.indexOf(value);
+    if(searchValue == 1){
+        todoArray.splice(searchValue, 1)
+    }
+
+    viewDones();
+}
+///------------------------------///
+
+/* ------------------------------ */
+/* -------- TA BORT TODO -------- */
+/* ------------------------------ */
+function remove(){
+    this.parentElement.remove();
+    
+    const todoBox = this.parentElement
+    const todoBoxParagraph = todoBox.getElementsByTagName('p')[0];
+    const value = todoBoxParagraph.textContent;
+    
+    var searchValue = todoArray.indexOf(value);
+    if(searchValue == 1){
+        todoArray.splice(searchValue, 1)
+    }
+    
+}
+///------------------------------///
+
+/* ------------------------------ */
+/* - TA BORT ALLA TODOS & DONES - */
+/* ------------------------------ */
+
+deleteAll.innerHTML=`<p id="deleteTodos">DELETE ALL TODOS</p>
+<p id="deleteDones">DELETE ALL DONES</p>`;
+
+const deleteTodos = document.getElementById('deleteTodos');
+
+const deleteDones = document.getElementById('deleteDones');
+
+
+deleteTodos.addEventListener('click', function(){
+    emptyTodos();
+})
+
+deleteDones.addEventListener('click', function(){
+    emptyDones();
+})
+
+function emptyTodos(){
+
+for(var i = todoArray.length; i > 0; i--){
+ 
+ todoArray.pop();
+ 
+} 
+    
+    viewTodo();
+        
 }
 
+function emptyDones(){
+
+    for(var i = doneArray.length; i > 0; i--){
+ 
+        doneArray.pop();
+ 
+    } 
+    
+    viewDones();
+        
+}
+///------------------------------///
 
 /* --------------------------- */
 /* -------- VISA TODOS ------- */
@@ -37,26 +126,64 @@ function addTodo(event){
 
 function viewTodo(){
 
-    let myTodos="";
+    let myTodos=""; 
+    i = 0;
 
     for (const todos of todoArray){
         myTodos += `
-            <div class="todoBox">
-            <p>${todos}</p>
-            <button id="todoCompleteButton" class="completeButton">v</button>
-            <button id="todoRemoveButton" class="removeButton">x</button>
+            <div class="todoBox" id=${i++}>
+                <p>${todos}</p>
+                <button id="todoCompleteButton" class="completeButton">v</button>
+                <button id="todoRemoveButton" class="removeButton">x</button>
             </div>
         ` 
+
     }
 
-    todoList.innerHTML = myTodos; 
+    todoList.innerHTML = myTodos;
+    
+    const completeButton = document.getElementsByClassName('completeButton');
+    
+    for(i = 0; i < completeButton.length; i++){
+        completeButton[i].addEventListener('click', completeTodo)
+    }
+    
+    
+    const removeButton = document.getElementsByClassName('removeButton');
+    
+    for(i = 0; i < removeButton.length; i++){
+        removeButton[i].addEventListener('click', remove)
+    }
+    
+
+
 
 }
-
-viewTodo();
-
 ///---------------------------///
 
- /* KNAPP FÖR ATT TA BORT ALLA TODOS */
+/* --------------------------- */
+/* ----- VISA AVKLARADE ------ */
+/* --------------------------- */
 
-////----------------------------------////
+function viewDones(){
+      let myDones=""; 
+    i = 0;
+
+    for (const dones of doneArray){
+        myDones += `
+            <div class="todoBox" id=${i++}>
+                <p>${dones}</p>
+                <button id="todoRemoveButton" class="removeButton">x</button>
+            </div>
+        ` 
+    }  
+    doneList.innerHTML = myDones;
+
+    
+    const removeButton = document.getElementsByClassName('removeButton');
+    
+    for(i = 0; i < removeButton.length; i++){
+        removeButton[i].addEventListener('click', remove)
+    }
+    
+}
